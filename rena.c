cc30c475ac6109b9655429c159f4511b1e759c16
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2021-08-10 17:53:43 +0700

    Create rena.c

---
+++ rena.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void){
+    // Let's challenge in my favorite language!!
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int arr[1000][1000];
+    for (int y = 0; y < b; y++){
+        for(int x = 0; x < a; x++){
+            scanf("%d", &arr[x][y]);
+        }
+    }
+    int out = 0;
+    for (int i = 0, q, w, e, r; i < c; i++){
+        scanf("%d %d %d %d", &q, &w, &e, &r);
+        q--;
+        w--;
+        do {
+            int t = q;
+            do {
+                out += arr[t][w];
+                arr[t][w] = 0;
+                t++;
+            } while (t < e);
+            w++;
+        } while (w < r);
+    }
+    printf("%d\n", out);
+    return 0;
+}
