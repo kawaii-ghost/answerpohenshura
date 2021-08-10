aada51453f21b5be7bfb9b56f0154e63a4b04000
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2021-08-10 17:55:11 +0700

    Update prob1.c

--- prob1.c
+++ prob1.c
@@ -3,9 +3,9 @@ int main(void){
     // Let's challenge in my favorite language!!
     char str[1000];
     fgets(str, sizeof(str), stdin);
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (i % 2 == 0) {
-            putchar(str[i]);
+    for (int i = 0; str[i] != '\0'; i++) { // Doing loop until find '\0'
+        if (i % 2 == 0) { // Check if i var is even
+            putchar(str[i]); // Print even index str array
         }
     }
     return 0;
