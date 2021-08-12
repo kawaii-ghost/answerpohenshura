94f09539ccf1448afc83739bd2bed59e14013a96
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2021-08-13 06:56:31 +0700

    Update prob1.c

--- prob1.c
+++ prob1.c
@@ -3,10 +3,13 @@ int main(void){
     // Let's challenge in my favorite language!!
     char str[1000];
     fgets(str, sizeof(str), stdin);
-    for (int i = 0; str[i] != '\0'; i++) { // Doing loop until find '\0'
-        if (i % 2 == 0) { // Check if i var is even
-            putchar(str[i]); // Print even index str array
+    //Didn't need to check if odd anymore if you add it two times (i += 2) :O
+    for (int i = 0; str[i] != '\0'; i += 2){ // Loop until find '\0'
+        putchar(str[i]);
+        if (str[i + 1] == '\0'){ // Case strlen(str) isn't odd
+            break;
         }
     }
+    puts("");
     return 0;
 }
