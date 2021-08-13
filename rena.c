5d40ea994c6ac915baddd24af4aae4edcda010c2
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2021-08-13 07:11:41 +0700

    Update rena.c
    Decrease statement

--- rena.c
+++ rena.c
@@ -4,25 +4,23 @@ int main(void){
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     int arr[1000][1000];
-    for (int y = 0; y < b; y++){
-        for(int x = 0; x < a; x++){
+    for (int y = 1; y <= b; y++){
+        for(int x = 1; x <= a; x++){
             scanf("%d", &arr[x][y]);
         }
     }
     int out = 0;
     for (int i = 0, q, w, e, r; i < c; i++){
         scanf("%d %d %d %d", &q, &w, &e, &r);
-        q--;
-        w--;
         do {
             int t = q;
             do {
                 out += arr[t][w];
                 arr[t][w] = 0;
                 t++;
-            } while (t < e);
+            } while (t <= e);
             w++;
-        } while (w < r);
+        } while (w <= r);
     }
     printf("%d\n", out);
     return 0;
