9e0174d7a56e8de65d0cfda5363716a5bfeed06e
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2021-10-08 16:59:21 +0700

    Update prob2.c

--- prob2.c
+++ prob2.c
@@ -2,8 +2,10 @@
 int main(void){
     // Let's challenge in my favorite language!!
     int a;
-    scanf("%d", &a); //Define how many input (Can be divided by 7)
-    int out[7] = {0}; //There's seven days in a week
+    scanf("%d", &a); // Define how many input (Can be divided by 7)
+    int out[7] = {0}; 
+    // There are seven days in a week 
+    // and initializing all of them to zero.
 
     for (int i = 0, b = 0, c; i < a; i++) {
         scanf("%d", &c);
