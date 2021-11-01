10346548febecdb3c85d3e7f2a12d4bd9a29a143
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2021-11-01 09:27:53 +0700

    Update prob1.c

--- prob1.c
+++ prob1.c
@@ -2,14 +2,29 @@
 int main(void){
     // Let's challenge in my favorite language!!
     char str[101];
+    /* 
+     * The maximal input is 100 character 
+     * + 1 for null character '\0'
+     */
+
     fgets(str, sizeof(str), stdin);
-    //Didn't need to check if odd anymore if you add it two times (i += 2) :O
-    for (int i = 0; str[i] != '\0'; i += 2){ // Loop until find '\0'
+    
+    
+    /* Loop until find '\0' */
+    
+    for (int i = 0; str[i] != '\0'; i += 2){
         putchar(str[i]);
-        if (str[i + 1] == '\0'){ // Case strlen(str) isn't odd
+        /*  
+         * Instead checking odd or even, 
+         * let's add the i var two times.
+         */
+        
+        if (str[i + 1] == '\0')
             break;
-        }
+        /* Case strlen(str) isn't odd */
     }
-    putchar('\n'); // Newline
+    
+    putchar('\n'); 
+    /* Newline */
     return 0;
 }
