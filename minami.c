92bacd7d1e877064bf757e61ff0f6ba2ef580e9f
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2021-11-01 09:51:21 +0700

    Update minami.c

--- minami.c
+++ minami.c
@@ -3,18 +3,23 @@
 int main(void) {
     int a, b;
     scanf("%d %d", &a, &b);
+    getchar();
+    
     int arr[20][20];
-    for (int i = 0; i < b; i++) {
-        for (int n = 0; n < a; n++) {
+    for (int i = 0, n; i < b; i++) {
+        for (n = 0; n < a; n++) {
             scanf("%d", &arr[n][i]);
-            if (arr[n][i] == 2) {
+            getchar();
+            
+            if (arr[n][i] == 2)
                 arr[n][i] = 0;
-            }
+                
         }
     }
-    for (int n = 0; n < a; n++) {
-        for (int i = b - 2; i >= 0; i--) {
-            int x = i;
+    for (int i, n = 0, x; n < a; n++) {
+        for (i = b - 2; i >= 0; i--) {
+            x = i;
+            
             do {
                 if (arr[n][x] == 1 && arr[n][x + 1] == 0) {
                     arr[n][x] = 0;
@@ -22,18 +27,16 @@ int main(void) {
                 } else {
                     break;
                 }
+                
                 x++;
             } while (x < (b - 1));
         }
     }
-    for (int i = 0; i < b; i++) {
-        for (int n = 0; n < a; n++) {
-            if (n == a - 1){
-                printf("%d\n", arr[n][i]);
-            } else {
-                printf("%d ", arr[n][i]);
-            }
+    for (int i = 0, n; i < b; i++) {
+        for (n = 0; n < a - 1; n++) {
+            printf("%d ", arr[n][i]);
         }
+        printf("%d\n", arr[n][i]);
     }
     return 0;
 }
