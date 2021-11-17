fb4edf35cd9179363973649d450fda108939d59f
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2021-11-17 10:48:52 +0700

    Add files via upload

--- minami.c
+++ minami.c
@@ -1,42 +1,65 @@
 #include <stdio.h>
-// Let's challenge in my favorite language!!
-int main(void) {
-    int a, b;
+/* Maximum array input is 20 x 20 */
+#define MAX 20
+
+int main(void)
+{
+    /* Let's challenge in my favorite language!! */
+    int a, b, i, n;
+    /*
+     * Actually I don't need pointer var
+     * but it's used for better readability
+     */
+    int *up, *down;
+    int arr[MAX][MAX];
+
     scanf("%d %d", &a, &b);
     getchar();
-    
-    int arr[20][20];
-    for (int i = 0, n; i < b; i++) {
+
+
+    for (i = 0; i < b; i++) {
         for (n = 0; n < a; n++) {
             scanf("%d", &arr[n][i]);
             getchar();
-            
+
             if (arr[n][i] == 2)
                 arr[n][i] = 0;
-                
         }
     }
-    for (int i, n = 0, x; n < a; n++) {
+
+    for (n = 0; n < a; n++) {
         for (i = b - 2; i >= 0; i--) {
-            x = i;
-            
+            b -= 1;
+
             do {
-                if (arr[n][x] == 1 && arr[n][x + 1] == 0) {
-                    arr[n][x] = 0;
-                    arr[n][x + 1] = 1;
+	        up = &arr[n][i];
+            	down = &arr[n][i + 1];
+
+                if (*up == 1 && *down == 0) {
+                    *up = 0;
+                    *down = 1;
                 } else {
                     break;
                 }
-                
-                x++;
-            } while (x < (b - 1));
+
+                i++;
+            } while (i < b);
+
+            b += 1;
         }
     }
-    for (int i = 0, n; i < b; i++) {
-        for (n = 0; n < a - 1; n++) {
+
+
+    for (i = 0; i < b; i++) {
+	a -= 1;
+
+        for (n = 0; n < a; n++) {
             printf("%d ", arr[n][i]);
         }
         printf("%d\n", arr[n][i]);
+
+        a += 1;
     }
+
     return 0;
 }
