91bb9a0efb47f3e9fe58d8740eec4b6a55bedb23
No Life <86765295+kawaii-ghost@users.noreply.github.com>
2022-06-01 06:26:21 +0700

    Update prob2.c

--- prob2.c
+++ prob2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <inttypes.h>
 
 /* There are 7 days in a week */
 #define WEEK 7
@@ -7,28 +7,26 @@
 int main(void)
 {
     /* Let's challenge in my favorite language!! */
-    uint_fast8_t n; 
-    int_fast8_t b = 0; 
-    int_fast16_t tmp;
-    /* Declare an array named arr and initialize 
-     * all the member to zero 
-     */
+    uint_fast8_t n;
+    int_fast8_t b = 0;
+    
     int_fast32_t arr[WEEK] = {0};
 
-    scanf("%d", &n);
+    scanf("%" SCNuFAST8, &n);
     getchar();
 
-    for (size_t i = 0; i < n; i++){
-        scanf("%d", &tmp);
+    for (size_t i = 0; i < n; i++) {
+        int_fast16_t tmp;
+        scanf("%" SCNdFAST16, &tmp);
         getchar();
 
         arr[b] += tmp;
-        /* Change b value into zero if b equals to 6 */
+     
         b == 6 ? b = 0 : b++;
     }
 
     for (size_t i = 0; i < WEEK; i++) {
-        printf("%d\n", arr[i]);
+        printf("%" PRIdFAST32 "\n", arr[i]);
     }
 
     return 0;
