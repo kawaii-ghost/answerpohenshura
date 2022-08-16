769009636d8fbcd0d94a5bac97a16ce2b6f42a67
kawaii-ghost <86765295+kawaii-ghost@users.noreply.github.com>
2022-08-16 13:53:19 +0700

    Fully Buffered

--- prob1.c
+++ prob1.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-/* Maximal character input is 100 char */
 #define MAX 100
 
 int main(void)
 {
     /* Let's challenge in my favorite language!! */
-
+    setvbuf(stdout, NULL, _IOFBF, 0);
     /* One more bytes for NULL character */
     char str[MAX + 1] = {'\0'};
 
     scanf("%100[^\n]", str);
     getchar();
 
-    for (size_t i = 0; str[i] != '\0'; i += 2){
-        putchar(str[i]);
+    for (char *ch = str; *ch != '\0'; ch++){
+        putchar(*ch);
     }
 
     putchar('\n');
-
-    return 0;
 }
